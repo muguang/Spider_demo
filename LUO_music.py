44a69ff327aa8db8a0ca6cb3b729e441484cd7d8
muguang <1132134963@qq.com>
2016-03-26 21:37:36 +0800

    add download info

--- LUO_music.py
+++ LUO_music.py
@@ -18,7 +18,7 @@ def get_song_list(volumn):
     r = requests.get(LUO_url.format(volumn))
     bs = BeautifulSoup(r.content, 'lxml')
     songs = bs.find_all('div', 'player-wrapper')
-    music_list = []
+    music_info = []
     cover_dic = {}
     for song in songs:
         # print(song)
@@ -31,14 +31,12 @@ def get_song_list(volumn):
         info.append(artist)
         info.append(album)
         # print(name, artist, album)
-        music_list.append(info)
+        music_info.append(info)
         cover_dic[album[7:]] = cover_url
-
-    return music_list, cover_dic
-
+    print("Debug: ", music_info)
+    return music_info, cover_dic
 
 def download_cover(cover_dic):
-    path = os.getcwd()
 
     for album_name, cover_url in cover_dic.items():
         suffix = re.findall('.+cover(.+)\?.+', cover_url)
@@ -46,34 +44,62 @@ def download_cover(cover_dic):
         # print(cover_name)
 
         with open(cover_name, "wb") as f:
-            time.sleep(1)
+            time.sleep(0.1)
             f.write(requests.get(cover_url).content)
-
+#
 
 def download_music(music_name):
     len_l = len(music_name)
 
     for i in range(1, len_l+1):
         track = "%02d" % i
-        music_url_temp = music_url.format(804, track)
+        music_url_temp = music_url.format(number, track)
         # print(music_url_temp)
-        print(music_name[i-1], music_url_temp)
-
-        with open(music_name[i-1]+".mp3", "wb") as f:
+        # print(music_name[i-1], music_url_temp)
+        music = music_name[i-1]+".mp3"
+        with open(music, "wb") as f:
+            print("downing: ", music)
             f.write(requests.get(music_url_temp).content)
+            time.sleep(0.1)
 
 
+def init(number):
 
-def main():
+
+    #  create the cataloge if not exist
+    #  if the cataloge have the content or is just a empty folder
+
+    path = os.getcwd()
+    path_name = path+"/"+str(number)
+    # print(path_name)
+    # print(type(path_name))
+
+    if not os.path.exists(path_name):
+        os.mkdir(path_name)
+    file_size = os.path.getsize(path_name)
+
+    # print(file_size)
+    # print(type(file_size))
+
+    if file_size > 100:
+        print("这期歌应该已经下载过了..")
+        exit()
+
+    os.chdir(path_name)
+    # print(os.getcwd())
+
     music_list, coverdict = get_song_list(number)
-    # print(music_list)
+
+    print(music_list, coverdict)
+
     music_name = []
     for music in music_list:
         music_name.append(music[0])
-    # download_music(music_name)
+    download_music(music_name)
+
     download_cover(cover_dic=coverdict)
 
 if __name__ == '__main__':
 
     number = input("下载哪一期音乐 >")
-    main(number)
+    init(number)
